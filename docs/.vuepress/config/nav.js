cd6c8979721ea42db5db0d5db3b735a330cde7f9
dt_flys <dt_flys@hotmail.com>
2022-07-13 00:09:46 +0800

    update

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,11 +1,11 @@
 module.exports = [
     { text: '首页', link: '/' },
-    { text: '文档', link: '/pages/intro/', items: [
-            { text: '1.5.x', link: '/pages/intro/'}
+    { text: '文档', link: '/pages/1.5.x/intro/', items: [
+            { text: '1.5.x', link: '/pages/1.5.x/intro/'}
         ]
     },
-    { text: '💖支持', link: '/pages/fb599d/' },
-    { text: '更新记录', link: '/pages/88c2d1/' },
+    { text: '💖支持', link: '/pages/donate/' },
+    { text: '更新记录', link: 'https://gitee.com/dromara/forest/releases' },
     { text: '关于项目', link: '/pages/8d7f1e/' },
     { text: '加入社区讨论', link: '/pages/73c2c3/' },
     { text: '参与开发', link: '/pages/ae4dd5/' },

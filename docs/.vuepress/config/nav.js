9b4443a7e71ef724d13a93ae21e1a8faa321df52
公子骏 <dt_flys@hotmail.com>
2023-03-13 15:28:40 +0800

    update: Forest 1.5.30

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,7 +1,7 @@
 module.exports = [
     { text: '💒 首页', link: '/' },
     { text: '📚 文档', link: '/pages/1.5.29/intro/', items: [
-            { text: 'v1.5.29', link: '/pages/1.5.29/intro/' },
+            { text: 'v1.5.30', link: '/pages/1.5.30/intro/' },
             { text: 'v1.5.28', link: '/pages/1.5.28/intro/' },
         ]
     },
